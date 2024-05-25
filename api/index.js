782f2cb117e46cc7dfb7e39148b23e89d48480c3
Rudviq <rudviq.bhavsar02@gmail.com>
2024-05-25 02:58:41 -0500

    Created controller and API route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
     console.log("Connected to MongoDB!");
 }).catch((err)=>{
     console.log(err);
@@ -15,3 +18,5 @@ const app=express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!!');
 });
+
+app.use('/api/user',userRouter);
