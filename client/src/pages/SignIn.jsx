8bf7a7c1b9e7852f8e5fdc865dc7eb1d17c5b9f1
Rudviq <rudviq.bhavsar02@gmail.com>
2024-06-25 22:01:02 -0400

    Added redux functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice';
+
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const [loading,setLoading] = useState(false);
-  const [error,setError] = useState(null);
+  const {loading,error} = useSelector((state) => state.user);
+  // const [loading,setLoading] = useState(false);
+  // const [error,setError] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) =>{
     setFormData({
@@ -19,7 +24,8 @@ export default function SignIn() {
     e.preventDefault();
     
     try{
-      setLoading(true);
+      dispatch(signInStart());
+      // setLoading(true);
      
       const res = await fetch('/api/auth/signin', {
           method: 'POST',
@@ -33,17 +39,21 @@ export default function SignIn() {
   
       
       if(data.success === false){
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
+
+        // setError(data.message);
+        // setLoading(false);
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
+      // setLoading(false);
+      // setError(null);
       navigate('/');
     }
     catch(error){
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(data.message));
+      // setLoading(false);
+      // setError(error.message);
     }
 
     
